cd6c6be2349dbea53f8a19fbcb4f8e77b794b713
张源睿 <768359509@qq.com>
2021-09-20 17:47:46 +0800

    3-5 集成mybatis generator,生成demo表代码，增加demo表列表查询

--- doc/all.sql
+++ doc/all.sql
@@ -1,9 +1,28 @@
 DROP TABLE IF EXISTS `test`;
-CREATE TABLE `test`  (
-                         `id` bigint NOT NULL,
-                         `name` varchar(50),
-                         `password` varchar(50),
-                         PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARACTER SET = utf8mb4;
+CREATE TABLE `test`
+(
+    `id`       bigint NOT NULL,
+    `name`     varchar(50),
+    `password` varchar(50),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4;
 
-SELECT * From test;
+INSERT INTO `test` (id, name, password)
+VALUES (1, '测试', 'password');
+
+SELECT *
+From test;
+
+DROP TABLE IF EXISTS `demo`;
+CREATE TABLE `demo`
+(
+    `id`   bigint NOT NULL,
+    `name` varchar(50),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  default
+      CHARACTER SET = utf8mb4;
+
+INSERT INTO `demo` (id, name)
+VALUES (1, '测试');
