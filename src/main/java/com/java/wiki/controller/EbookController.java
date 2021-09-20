d3004ae65222e165f7b01e0921f781bb8a0ddeeb
张源睿 <768359509@qq.com>
2021-09-20 21:13:38 +0800

    3-6 后端接口返回类型统一为CommonResp

--- src/main/java/com/java/wiki/controller/EbookController.java
+++ src/main/java/com/java/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.java.wiki.controller;
 
 import com.java.wiki.entity.Ebook;
+import com.java.wiki.resp.CommonResp;
 import com.java.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,11 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp<List<Ebook>> list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
 }
