7037083c62e84b30b4722223256c182f47153084
张源睿 <768359509@qq.com>
2021-09-20 17:49:42 +0800

    3-5 集成mybatis generator,生成demo表代码，增加demo表列表查询

---
+++ src/main/java/com/java/wiki/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.java.wiki.controller;
+
+import com.java.wiki.entity.Demo;
+import com.java.wiki.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Resource
+    private DemoService demoService;
+
+
+    @GetMapping("/list")
+    public List<Demo> list() {
+        return demoService.list();
+    }
+
+}
---
+++ src/main/java/com/java/wiki/service/DemoService.java
@@ -0,0 +1,23 @@
+package com.java.wiki.service;
+
+
+import com.java.wiki.entity.Demo;
+import com.java.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+
+
+}
