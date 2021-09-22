f407fabcfd5f4711aa344274265a09e7aeedcfc3
张源睿 <768359509@qq.com>
2021-09-22 14:09:06 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,19 +2,19 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      加瓦电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
 import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
+import TheFooter from '@/components/the-footer.vue'; // @ is an alias to /src
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter,
   },
 });
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    加瓦电子书
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
