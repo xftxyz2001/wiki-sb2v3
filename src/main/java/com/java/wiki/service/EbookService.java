3bbd9ee817dc9bde8973677e0c99aed58c8272b6
张源睿 <768359509@qq.com>
2021-09-27 17:58:48 +0800

    6-4 集成PageHelper插件实现后端分页，修改mapper日志级别

--- src/main/java/com/java/wiki/service/EbookService.java
+++ src/main/java/com/java/wiki/service/EbookService.java
@@ -1,12 +1,16 @@
 package com.java.wiki.service;
 
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.java.wiki.entity.Ebook;
 import com.java.wiki.entity.EbookExample;
 import com.java.wiki.mapper.EbookMapper;
 import com.java.wiki.req.EbookReq;
 import com.java.wiki.resp.EbookResp;
 import com.java.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -16,19 +20,26 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
 
     public List<EbookResp> list(EbookReq req) {
+
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if (!StringUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
 
-
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
 
         return CopyUtil.copyList(ebookList, EbookResp.class);
 
