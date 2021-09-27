0af8f49e7b8f309051e286aaf2438bbd5ce47e00
张源睿 <768359509@qq.com>
2021-09-27 11:20:02 +0800

    6-2 页面命名改为小写，新增电子书管理页面，修改路由，header菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,25 +1,34 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import Home from '../views/home.vue'
+import About from '../views/about.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
+
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        //component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+        component: About
+    },
+    {
+        path: '/admin/ebook',
+        name: 'AdminEbook',
+        component: AdminEbook
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
