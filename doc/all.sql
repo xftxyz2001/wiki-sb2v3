ae4d4f7254bad9ac4940a8aac2a0c53456dd8bb8
张源睿 <768359509@qq.com>
2021-09-28 23:25:07 +0800

    7-2 使用mybatis-generator生成分类表持久层代码 无限级树型表结构设计：id+parent

--- doc/all.sql
+++ doc/all.sql
@@ -26,7 +26,21 @@ values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用
 INSERT INTO `ebook`(id, name, description)
 values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发');
 
-
+insert into `category` (id, parent, name, sort) values (100, 000, '前端开发', 100);
+insert into `category` (id, parent, name, sort) values (101, 100, 'Vue', 101);
+insert into `category` (id, parent, name, sort) values (102, 100, 'HTML & CSS', 102);
+insert into `category` (id, parent, name, sort) values (200, 000, 'Java', 200);
+insert into `category` (id, parent, name, sort) values (201, 200, '基础应用', 201);
+insert into `category` (id, parent, name, sort) values (202, 200, '框架应用', 202);
+insert into `category` (id, parent, name, sort) values (300, 000, 'Python', 300);
+insert into `category` (id, parent, name, sort) values (301, 300, '基础应用', 301);
+insert into `category` (id, parent, name, sort) values (302, 300, '进阶方向应用', 302);
+insert into `category` (id, parent, name, sort) values (400, 000, '数据库', 400);
+insert into `category` (id, parent, name, sort) values (401, 400, 'MySQL', 401);
+insert into `category` (id, parent, name, sort) values (500, 000, '其它', 500);
+insert into `category` (id, parent, name, sort) values (501, 500, '服务器', 501);
+insert into `category` (id, parent, name, sort) values (502, 500, '开发工具', 502);
+insert into `category` (id, parent, name, sort) values (503, 500, '热门服务端语言', 503);
 
 DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test`
