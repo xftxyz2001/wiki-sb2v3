2dccb472dfc2ec40e5abbca1c42a0c30f5b08c27
张源睿 <768359509@qq.com>
2021-10-06 13:57:18 +0800

    8-4 TreeSelect 树选择组件，选择父节点 递归算法将当前目标节点及其子节点设置为disabled 为选择树添加一个"无"

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -136,6 +136,8 @@ export default defineComponent({
      **/
     const handleQuery = () => {
       loading.value = true;
+      //清空现有数据
+      level1.value = [];
       axios.get("/category/all").then((response) => {
         loading.value = false;
         const data = response.data;
