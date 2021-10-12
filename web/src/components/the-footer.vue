4b800db903ed90526a91f723065aad6bcaaab22d
张源睿 <768359509@qq.com>
2021-10-12 15:27:09 +0800

    9-9 使用vuex保存用户信息，方便其它组件获取登录信息

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,12 +1,22 @@
 <template>
   <a-layout-footer style="text-align: center">
-    加瓦电子书
+    加瓦电子书，欢迎：{{ user.name }}
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import store from '@/store';
+import {computed, defineComponent} from 'vue';
+
 export default defineComponent({
   name: 'the-footer',
+  setup() {
+    const user = computed(() =>
+        store.state.user
+    );
+    return {
+      user,
+    }
+  }
 });
 </script>
