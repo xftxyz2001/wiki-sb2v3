71383b82efa85c892989b8b4b3eeb79faa3d5500
张源睿 <768359509@qq.com>
2021-10-13 10:41:59 +0800

    9-12 前后端增加登录拦截 修复BUG：token类型转换，前端string，redis后端long

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,7 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import axios from "axios";
+import {Tool} from "@/util/tool";
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
@@ -13,6 +14,11 @@ axios.defaults.baseURL = process.env.VUE_APP_SERVER;
  */
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
+    const token = store.state.user.token;
+    if (Tool.isNotEmpty(token)) {
+        config.headers.token = token;
+        console.log("请求headers增加token：", token);
+    }
     return config;
 }, error => {
     return Promise.reject(error);
